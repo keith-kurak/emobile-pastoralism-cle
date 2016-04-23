e206cc77a83a69cb386051cd5fa92cf445855f28
Jennifer Evans <jenevans3ten@gmail.com>
2016-04-23 11:15:30 -0400

    Initial sample data

---
+++ www/sample_data/cleveland.js
@@ -0,0 +1,19 @@
+var clevelandData = [
+  {
+    isBurst: false,  
+    burstDuration: 0,
+    priority: 1,
+    title: 'Fairview Hospital',
+    type: 'hospital',  //matches icon
+    address1: '',
+    address2: '',
+    phone: '',
+    mapUrl: '',
+    detailImgUrl: '',
+    detailText: '',
+    locationDesc: '',
+    iconUrl: '',
+  },
+
+
+]
