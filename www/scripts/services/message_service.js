ff9775c16025c38baba9dcfc8d52429a29e36796
Keith Kurak <keith.kurak@gmail.com>
2016-04-23 15:12:47 -0400

    sample data is json-ified

--- www/scripts/services/message_service.js
+++ www/scripts/services/message_service.js
@@ -5,13 +5,23 @@ angular.module('yomadApp')
 
     return {
       getMessagesForArea(area) {
-        return $q.when(clevelandData).then(function(messages) {
+        var deferred = $q.defer();
+        $.getJSON("sample_data/global.json", function(json) {
+          var messages = json.locations[0].messages;
+          for(var m in messages) {
+            var message = messages[m];
+            message.localIconPath = iconService.getIconPathForMessage(message);
+          }
+          return deferred.resolve(messages);
+        });
+        return deferred.promise;
+        /*return $q.when(clevelandData).then(function(messages) {
           for(var m in messages) {
             var message = messages[m];
             message.localIconPath = iconService.getIconPathForMessage(message);
           }
           return $q.when(messages);
-        });
+        });*/
       }
     }
 
