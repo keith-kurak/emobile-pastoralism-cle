20f8e93a1c4f28e4a12dd3421c0b17cdaca2d41b
Keith Kurak <keith.kurak@gmail.com>
2016-04-23 11:47:46 -0400

    angular iterating through messages on main controller

--- www/scripts/controllers/main.js
+++ www/scripts/controllers/main.js
@@ -2,6 +2,5 @@
 
 angular.module('yomadApp')
   .controller('MainCtrl', ['$scope', '$window', '$q', function ($scope, $window, $q) {
-    
-    $scope.message = "here's a message from angular, do you like it?";
+    $scope.messages = clevelandData;
   }]);
