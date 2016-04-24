55dd22a4751fb0f7e7c2d5757c7aaeb7973951eb
Keith Kurak <keith.kurak@gmail.com>
2016-04-24 11:53:04 -0400

    submission detail appears

--- www/scripts/controllers/submit_detail.js
+++ www/scripts/controllers/submit_detail.js
@@ -3,9 +3,9 @@
 angular.module('yomadApp')
   .controller('SubmitDetailCtrl', ['$scope', '$window', '$q', '$location', 'submissionService', '$routeParams', function ($scope, $window, $q, $location, submissionService, $routeParams) {
     
-    submissionService.getMessageTypes().then(function(messageTypes) {
-      $scope.currentMessageCategory = _.find(messageTypes, function(type, key) {
-        return $routeParams.messageCategoryId === key;
+    submissionService.getMessageCategories().then(function(messageCategories) {
+      $scope.currentMessageCategory = _.find(messageCategories, function(messageCategory) {
+        return $routeParams.messageCategoryId === messageCategory.categoryId;
       });
     });
 
