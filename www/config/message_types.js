6c44a17bfd6e1e1d9333055ea38a599f8c9e39d8
Jennifer Evans <jenevans3ten@gmail.com>
2016-04-24 10:54:21 -0400

    First attempt- is this syntax correct?

--- www/config/message_types.js
+++ www/config/message_types.js
@@ -1,4 +1,39 @@
-var message_types = {
+var message_types = [
+  {
+    burstID = "weather",
+    burstName = "Weather",
+    color = "red"[
+      {
+        opID = "wind",
+        opName = "High Wind",
+        opPriority = 6,
+        color = "red"
+      },
+      {
+        opID = "snow",
+        opName = "Snowstorm",
+        opPriority = 8,
+        color = "red"
+      }
+    ]},
 
-  
-}
+  {
+    burstID = "warning",
+    burstName = "Warning",
+    color = "red"},
+
+  {
+    burstID = "land",
+    burstName = "Land",
+    color = "green"},
+
+  {
+    burstID = "market",
+    burstName = "Market",
+    color = "green"},
+
+  {
+    burstID = "info",
+    burstName = "Information",
+    color = "blue"}
+]
