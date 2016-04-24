d7a54b323110bc8540043b8b8272fa4ea5f0c1a2
Jennifer Evans <jenevans3ten@gmail.com>
2016-04-24 11:29:13 -0400

    Fixed syntax = to :

--- www/config/message_types.js
+++ www/config/message_types.js
@@ -1,101 +1,87 @@
 var message_categories = [
   {
-    categoryId = "weather",
-    name = "Weather",
-    color = "red",
+    categoryId : "weather",
+    name : "Weather",
+    color : "red",
     options: [
       {
-        opID = "weather-wind",
-        opName = "High Wind",
-        opPriority = 6,
-        color = "red"
+        opID : "weather-wind",
+        opName : "High Wind",
+        opPriority : 6,
+        color : "red"
       },
       {
-        opID = "weather-snow",
-        opName = "Snowstorm",
-        opPriority = 8,
-        color = "red"
+        opID : "weather-snow",
+        opName : "Snowstorm",
+        opPriority : 8,
+        color : "red"
       }
     ]
   },
 
   {
-    burstID = "warning",
-    name = "Warning",
-    color = "red",
+    burstID : "warning",
+    name : "Warning",
+    color : "red",
     options: [
       {
-        opID = "warning-theft",
-        opName = "Theft",
-        opPriority = 10,
-        color = "red"
+        opID : "warning-theft",
+        opName : "Theft",
+        opPriority : 10,
+        color : "red"
       },
       {
-        opID = "warning-badroute",
-        opName = "Bad Route",
-        opPriority = 5,
-        color = "red"
+        opID : "warning-badroute",
+        opName : "Bad Route",
+        opPriority : 5,
+        color : "red"
       }
     ]
   },
 
   {
-    burstID = "land",
-    name = "Land",
-    color = "green",
+    burstID : "land",
+    name : "Land",
+    color : "green",
     options: [
       {
-        opID = "land-grass",
-        opName = "High Grass",
-        opPriority = 2,
-        color = "green"
+        opID : "land-grass",
+        opName : "High Grass",
+        opPriority : 2,
+        color : "green"
       },
       {
-        opID = "land-drought",
-        opName = "Drought",
-        opPriority = 8,
-        color = "red"
+        opID : "land-drought",
+        opName : "Drought",
+        opPriority : 8,
+        color : "red"
       }
     ]
   },
 
   {
-    burstID = "market",
-    name = "Market",
-    color = "green",
+    burstID : "market",
+    name : "Market",
+    color : "green",
     options: [
       {
-        opID = "market-wool",
-        opName = "Wool Trade",
-        opPriority = 2,
-        color = "green"
+        opID : "market-wool",
+        opName : "Wool Trade",
+        opPriority : 2,
+        color : "green"
       },
       {
-        opID = "market-herbs",
-        opName = "Medicinal Herbs",
-        opPriority = 2,
-        color = "green"
+        opID : "market-herbs",
+        opName : "Medicinal Herbs",
+        opPriority : 2,
+        color : "green"
       }
     ]
   },
 
   {
-    burstID = "info",
-    name = "Information",
-    color = "blue",
-    options: [
-      {
-        opID = "info-loc",
-        opName = "My Location",
-        opPriority = 1,
-        color = "blue"
-      },
-      {
-        opID = "info-meet",
-        opName = "Meeting Location",
-        opPriority = 8,
-        color = "blue"
-      }
-    ]
+    burstID : "map",
+    name : "Location",
+    color : "blue"
   }
 ]
