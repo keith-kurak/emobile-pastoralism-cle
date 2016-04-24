f557679f9f1a97439e95ebb5ef83d421482c558a
Jennifer Evans <jenevans3ten@gmail.com>
2016-04-24 14:13:52 -0400

    tweaks

--- www/scripts/services/message_description_service.js
+++ www/scripts/services/message_description_service.js
@@ -13,7 +13,7 @@ angular.module('yomadApp')
         }
 
         if(message.isBurst && message.type === "map") {
-          return pMsg + message.user + ": " + message.locationDesc;
+          return pMsg + message.user + ": " + message.name;
         } else if(message.isBurst && message.type == "land") {
           return pMsg + message.user + ": " + message.name + " at " + message.locationDesc;
         } else if(message.isBurst) {
