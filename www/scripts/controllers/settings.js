f974ebb248c4b4012e28585b561a93e06ecdad08
Keith Kurak <keith.kurak@gmail.com>
2016-04-24 01:41:52 -0400

    settings are done

--- www/scripts/controllers/settings.js
+++ www/scripts/controllers/settings.js
@@ -3,6 +3,12 @@
 angular.module('yomadApp')
   .controller('SettingsCtrl', ['$scope', '$window', '$q', '$location', function ($scope, $window, $q, $location) {
 
+    $scope.username = $window.localStorage.getItem('username')
+
+    $scope.backClicked = function() {
+      $window.localStorage.setItem('username', $scope.username);
+      $location.path('/');
+    }
     
 
   }]);
