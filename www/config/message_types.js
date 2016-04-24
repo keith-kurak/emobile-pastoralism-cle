01ca57b54d88a435315cfef25c47586c536776a0
Jennifer Evans <jenevans3ten@gmail.com>
2016-04-24 11:13:38 -0400

    Initial msg_types- does it work?

--- www/config/message_types.js
+++ www/config/message_types.js
@@ -1,7 +1,7 @@
 var message_categories = [
   {
     categoryId = "weather",
-    burstName = "Weather",
+    name = "Weather",
     color = "red",
     options: [
       {
@@ -11,7 +11,7 @@ var message_categories = [
         color = "red"
       },
       {
-        opID = "snow",
+        opID = "weather-snow",
         opName = "Snowstorm",
         opPriority = 8,
         color = "red"
@@ -21,21 +21,81 @@ var message_categories = [
 
   {
     burstID = "warning",
-    burstName = "Warning",
-    color = "red"},
+    name = "Warning",
+    color = "red",
+    options: [
+      {
+        opID = "warning-theft",
+        opName = "Theft",
+        opPriority = 10,
+        color = "red"
+      },
+      {
+        opID = "warning-badroute",
+        opName = "Bad Route",
+        opPriority = 5,
+        color = "red"
+      }
+    ]
+  },
 
   {
     burstID = "land",
-    burstName = "Land",
-    color = "green"},
+    name = "Land",
+    color = "green",
+    options: [
+      {
+        opID = "land-grass",
+        opName = "High Grass",
+        opPriority = 2,
+        color = "green"
+      },
+      {
+        opID = "land-drought",
+        opName = "Drought",
+        opPriority = 8,
+        color = "red"
+      }
+    ]
+  },
 
   {
     burstID = "market",
-    burstName = "Market",
-    color = "green"},
+    name = "Market",
+    color = "green",
+    options: [
+      {
+        opID = "market-wool",
+        opName = "Wool Trade",
+        opPriority = 2,
+        color = "green"
+      },
+      {
+        opID = "market-herbs",
+        opName = "Medicinal Herbs",
+        opPriority = 2,
+        color = "green"
+      }
+    ]
+  },
 
   {
     burstID = "info",
-    burstName = "Information",
-    color = "blue"}
+    name = "Information",
+    color = "blue",
+    options: [
+      {
+        opID = "info-loc",
+        opName = "My Location",
+        opPriority = 1,
+        color = "blue"
+      },
+      {
+        opID = "info-meet",
+        opName = "Meeting Location",
+        opPriority = 8,
+        color = "blue"
+      }
+    ]
+  }
 ]
