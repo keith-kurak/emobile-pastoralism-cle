18960a2e1c674c9cae8f6b1dfa9e63a1fb06ab6f
Keith Kurak <keith.kurak@gmail.com>
2016-04-25 11:05:05 -0400

    Update app.js

--- www/scripts/app.js
+++ www/scripts/app.js
@@ -42,26 +42,3 @@
         redirectTo: '/'
       });
   })
-  /*.run(function ($rootScope, $location, $window) {
-    // Handle Android back button press - not sure if this will work, but will leave it just in case
-    if(navigator.userAgent.match(/(Android)/)) {
-      document.addEventListener("backbutton", function(e){
-        if($location.$$path != '/') {
-          $rootScope.$emit('backPressed');
-          e.preventDefault();
-          return false;
-        }
-      }, false);
-    }
-
-    // Handle Windows Phone back button press - not sure if this will work, but will leave it just in case
-    else if(navigator.userAgent.match(/(IEMobile)/)) {
-      var hardwareButtons = Windows.Phone.UI.Input.HardwareButtons;
-      hardwareButtons.addEventListener("backpressed", function (e) {
-         if($location.$$path != '/') {
-          $rootScope.$emit('backPressed');
-          e.handled = true;
-         }
-      });
-    }
-});*/
